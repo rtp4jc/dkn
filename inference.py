fba52b164a1a05df0df452e281c168e6ab8e25ab
Ryan Peterson <rtp4jc@gmail.com>
2022-03-28 07:12:40 -0500

    Attempt and fail to replace Kernel DKN with Swin Transformer

--- inference.py
+++ inference.py
@@ -4,27 +4,43 @@ import cv2
 import argparse
 
 from models import *
-
-
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--rgb',  default='images/0_rgb.png', help='name of rgb image')
-parser.add_argument('--depth',  default='images/0_lr.png', help='name of low resolution depth image')
+parser.add_argument(
+    '--rgb',
+    default='images/0_rgb.png',
+    help='name of rgb image'
+    )
+parser.add_argument(
+    '--depth',
+    default='images/0_lr.png',
+    help='name of low resolution depth image'
+    )
 parser.add_argument('--k', type=int, default=3, help='size of kernel')
 parser.add_argument('--d', type=int, default=15, help='size of grid area')
 parser.add_argument('--scale', type=int, default=8, help='scale factor')
-parser.add_argument('--parameter',  default='parameter/FDKN_8x', help='name of parameter file')
-parser.add_argument('--model',  default='FDKN', help='choose model FDKN or DKN')
-parser.add_argument('--output',  default='images/0_dkn.png', help='name of output image')
+parser.add_argument(
+    '--parameter',
+    default='parameter/FDKN_8x',
+    help='name of parameter file'
+    )
+parser.add_argument('--model', default='FDKN', help='choose model FDKN or DKN')
+parser.add_argument(
+    '--output',
+    default='images/0_dkn.png',
+    help='name of output image'
+    )
 opt = parser.parse_args()
 print(opt)
+
 
 def modcrop(image, modulo):
     h, w = image.shape[0], image.shape[1]
     h = h - h % modulo
     w = w - w % modulo
 
-    return image[:h,:w]
+    return image[:h, :w]
 
 
 if opt.model == 'FDKN':
@@ -34,23 +50,36 @@ elif opt.model == 'DKN':
 
 net.load_state_dict(torch.load(opt.parameter))
 net.eval()
-print('parameter \"%s\" has loaded'%opt.parameter)
+print('parameter \"%s\" has loaded' % opt.parameter)
 
 
-rgb = cv2.imread(opt.rgb).astype('float32')/255.0
+rgb = cv2.imread(opt.rgb).astype('float32')
+plt.title('high res reference')
+plt.imshow(rgb)
+plt.show()
+rgb = rgb / 255.0
 rgb = modcrop(rgb, opt.scale)
-rgb = np.transpose(rgb, (2,0,1))
+rgb = np.transpose(rgb, (2, 0, 1))
 
-lr = cv2.imread(opt.depth,cv2.IMREAD_GRAYSCALE).astype('float32')/255.0
+lr = cv2.imread(opt.depth, cv2.IMREAD_GRAYSCALE).astype('float32')
+
+plt.title('low res target')
+plt.imshow(lr, cmap='gray')
+plt.show()
+
+lr = lr / 255.0
 lr = modcrop(lr, opt.scale)
 lr = np.expand_dims(lr, 0)
 
-image = torch.from_numpy(np.expand_dims(rgb,0)).cuda()
-depth = torch.from_numpy(np.expand_dims(lr,0)).cuda()
-
-
+image = torch.from_numpy(np.expand_dims(rgb, 0)).cuda()
+depth = torch.from_numpy(np.expand_dims(lr, 0)).cuda()
 
 with torch.no_grad():
     res_img = net((image, depth)).cpu().numpy()
-    
-cv2.imwrite(opt.output, res_img[0,0]*255)
+
+output_image = res_img[0, 0] * 255
+cv2.imwrite(opt.output, output_image)
+
+plt.title('super-resolution target')
+plt.imshow(output_image, cmap='gray')
+plt.show()
--- nyu_dataloader.py
+++ nyu_dataloader.py
@@ -8,17 +8,20 @@ from torchvision import transforms, utils
 class NYU_v2_datset(Dataset):
     """NYUDataset."""
 
-    def __init__(self, root_dir, scale=8, train=True, transform=None):
+    def __init__(self, root_dir, scale=8, train=True, color_transform=None,
+                 depth_transform=None):
         """
         Args:
             root_dir (string): Directory with all the images.
             scale (float): dataset scale
             train (bool): train or test
-            transform (callable, optional): Optional transform to be applied on a sample.
+            color_transform (callable, optional): Optional transform to be
+            applied on a sample.
             
         """
         self.root_dir = root_dir
-        self.transform = transform
+        self.color_transform = color_transform
+        self.depth_transform = depth_transform
         self.scale = scale
         
         if train:
@@ -39,10 +42,10 @@ class NYU_v2_datset(Dataset):
         s = self.scale
         target = np.array(Image.fromarray(depth).resize((w//s,h//s),Image.BICUBIC).resize((w, h), Image.BICUBIC))
 
-        if self.transform:
-            image = self.transform(image).float()
-            depth = self.transform(np.expand_dims(depth,2)).float()
-            target = self.transform(np.expand_dims(target,2)).float()
+        if self.color_transform:
+            image = self.color_transform(image).float()
+            depth = self.depth_transform(np.expand_dims(depth, 2)).float()
+            target = self.depth_transform(np.expand_dims(target, 2)).float()
 
         sample = {'guidance': image, 'target': target, 'gt': depth}
         
